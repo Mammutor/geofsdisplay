14b345768925182994a6bd96d1529995aecef24d
mammutor <mammutor@gmail.com>
2025-06-01 14:37:24 +0200

    reverted regenradargif.php, still not working

--- php/regenradargif.php
+++ php/regenradargif.php
@@ -1,108 +1,70 @@
 <?php
-// PHP-Fehlerprotokollierung und -anzeige erzwingen
-error_reporting(E_ALL);        // Alle Fehler und Warnungen anzeigen
-ini_set('display_errors', 1);  // Fehler direkt im Browser anzeigen
-ini_set('display_startup_errors', 1); // Fehler beim PHP-Start anzeigen
-ini_set('log_errors', 1);      // Fehler in die Log-Datei schreiben
-ini_set('error_log', '/proc/self/fd/2'); // Fehler in den Standard-Error-Stream schreiben (für Docker logs)
-
-// Header setzen
-header('Content-Type: image/gif');
-
-// GIFEncoder-Klasse einbinden (bitte achte darauf, dass diese Datei im gleichen Ordner liegt!)
+// GIF code example: http://www.jeroenvanwissen.nl/weblog/php/howto-generate-animated-gif-with-php
+header ('Content-type:image/gif');
+// class from here: http://www.phpclasses.org/browse/package/3163.html
+// deep download link: http://www.phpclasses.org/browse/download/zip/package/3163/name/gifmerge-2007-09-25.zip
 include('GIFEncoder.class.php');
 
-// HTTP-Kontext mit User-Agent
-$options = array('http' => array(
-    'method' => "GET",
-    'header' => "User-Agent: geofsdisplay v1.0\r\n"
-));
+// add user agent to be fair
+// http://stackoverflow.com/questions/2107759/php-file-get-contents-and-headers
+$options = array('http'=>array('method'=>"GET", 'header'=>"User-Agent: geofsdisplay v1.0\r\n"));
 $context = stream_context_create($options);
 
-// Daten vom WetterOnline-Server holen
-$apiUrl = 'https://tiles.wo-cloud.com/metadata?lg=wr&period=periodCurrentLowRes';
-$dataJson = @file_get_contents($apiUrl, false, $context);
-if ($dataJson === false) {
-    $error = error_get_last();
-    die('Fehler beim Laden der Wetterdaten von ' . $apiUrl . '. Details: ' . (isset($error['message']) ? $error['message'] : 'Unbekannter Fehler.'));
-}
-$data = json_decode($dataJson);
+// get current data from wetteronline.de API endpoint
+$data = json_decode(file_get_contents('https://tiles.wo-cloud.com/metadata?lg=wr&period=periodCurrentLowRes', false, $context));
 
-if (json_last_error() !== JSON_ERROR_NONE) {
-    die('Fehler beim Dekodieren der JSON-Daten: ' . json_last_error_msg());
-}
+$frames=[];
+$dauer=[];
 
-if (!isset($data->liveid) || !isset($data->timesteps) || empty($data->timesteps)) { // Hinzugefügt: empty($data->timesteps)
-    die('Ungültiges Datenformat von der API. Erwartete "liveid" oder "timesteps" fehlen oder "timesteps" ist leer.');
-}
-
-// Zeitzonen-Offset korrekt extrahieren
-$liveidParts = explode('-', $data->liveid);
-$timezoneoffset = isset($liveidParts[2]) ? intval($liveidParts[2]) : 0;
-
-$frames = [];
-$dauer = [];
-
-foreach ($data->timesteps as $step) {
-    $frameCounter = 0;
-    $frameCounter++;
-    // URL-Zusammenstellung für das Bild
+foreach($data->timesteps as $step)
+{
+    // build tiles parameter. this is how Christoph found the necessary content for this parameter:
+    //   1. open browser console -> go to https://www.wetteronline.de/regenradar/nordrhein-westfalen -> observe network traffic
+    //   2. look at images starting with "composite" -> select the one that has Münster in it (look for NRW border around Osnabrück)
+    //   3. copy `tiles` parameter -> base64_decode (function `atob` in JavaScript) -> you get this long string starting with "topo|"
+    //   4. replace variables in path as seen below -> remove "_cities_excluded" from "rr_geooverlay" so that city names are INcluded
+    //   -> that's it!
+    // note that "ZL" stands for "zoom level" and that (66,42) and (132,84) correspond to Münster's tile numbers (when rounded) in the
+    // "slippy tile names" scheme (https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames) in zoom level 7 and 8 respectively.
     $tiles = 'topo|1;;0;0|wetterradar/prozess/tiles/geolayer/rasterimages/rr_topography/v1/ZL8/512/132_84.jpg$r|2;;0;0;false' .
         '|' . $step->layers->europe->rain->ptypPath . $step->layers->europe->rain->path . '/' . implode('/', $step->layers->europe->rain->timePath) . '/ZL7/522/sprite/66_42.png' .
         ';' . $step->layers->global->rain->ptypPath . $step->layers->global->rain->path . '/' . implode('/', $step->layers->global->rain->timePath) . '/ZL7/522/border/66_42.png$i' .
         '|1;;0;0|geo/prozess/karten/produktkarten/wetterradar/generate/rasterTiles/rr_geooverlay/v2/ZL8/512/132_84.png';
-
+    // build URL (parameters `k` and `time` left out as they don't seem to be necessary, "rr" probably stands for "rain radar")
     $url = 'https://tiles.wo-cloud.com/composite?format=png&lg=rr&tiles=' . urlencode(base64_encode($tiles));
 
-    error_log("Versuche Bild $frameCounter abzurufen von: $url");
-    $datei = @file_get_contents($url, false, $context);
-    if ($datei === false) {
-        $error = error_get_last();
-        error_log("FEHLER: Beim Abrufen des Bildes $frameCounter von: $url. Details: " . (isset($error['message']) ? $error['message'] : 'Unbekannter Fehler.'));
-        continue; // Wichtig: Mit continue geht es zum nächsten Frame
-    }
+    // retrieve PNG from wetteronline.de API
+    $datei = file_get_contents($url);
+    // Turn the data into an image
+    $image = imagecreatefromstring($datei);
 
-    $image = @imagecreatefromstring($datei);
-    if ($image === false) {
-        error_log("FEHLER: Beim Erzeugen des Bildes $frameCounter aus den Daten (vermutlich kein gültiges Bildformat). URL: $url");
-        continue;
-    }
+    // Crop away Düsseldorf and Köln at the bottom so that Münster is vertically centered
+    $cropped = imagecrop($image, ['x' => 0, 'y' => 0, 'width' => 512, 'height' => 365]);
 
-    $cropped = @imagecrop($image, ['x' => 0, 'y' => 0, 'width' => 512, 'height' => 365]);
-    if ($cropped === false) {
-        error_log("FEHLER: Beim Croppen des Bildes $frameCounter. URL: $url");
-        imagedestroy($image);
-        continue;
-    }
+    // Add the frame's timestamp as text below the "Münster" label
+    // 1. Extract the timestamp
+    $timezoneoffset = $data->liveid[14];   // `liveid` has format "20210714-1640-2" -- I *assume* the last bit is the timezone
+    $hour = ($step->layers->europe->rain->timePath[3] + $timezoneoffset) % 24;   // `timePath` has format ["2021","07","14","16","40","v11"]
+    $time = ($hour < 10 ? '0' : '') . $hour . ':' . $step->layers->europe->rain->timePath[4];   // leading zero, hour, colon, minute
+    // 2. Choose a text color
+    $textcolor = imagecolorallocate($cropped, 0, 0, 0);   // nice and simple black
+    // 3. Write onto the image (5 uses the biggest font size, (342,178) puts it under the "Münster" label, use (450,345) for bottom right corner)
+    imagestring($cropped, 5, 342, 178, $time, $textcolor);
 
-    // Zeit extrahieren und darstellen
-    $hour = ((int)$step->layers->europe->rain->timePath[3] + $timezoneoffset) % 24;
-    $minute = $step->layers->europe->rain->timePath[4];
-    $time = sprintf('%02d:%s', $hour, $minute);
-
-    $textcolor = imagecolorallocate($cropped, 0, 0, 0); // Schwarz
-    imagestring($cropped, 5, 342, 178, $time, $textcolor); // Zeitstempel einfügen
-
-    // GIF-Frame erzeugen
+    // save frame
     ob_start();
-    imagegif($cropped);
-    $frames[] = ob_get_clean();
-    $dauer[] = 100; // Dauer pro Frame (100 = 1 Sekunde)
-
-    // Speicher aufräumen
-    imagedestroy($image);
-    imagedestroy($cropped);
+    imagegif($cropped);   // convert to GIF
+    $frames[]=ob_get_contents();
+    $dauer[]=100;
+    ob_end_clean();
 }
 
-// Letzten Frame länger anzeigen (Ende besser sichtbar)
-if (!empty($frames)) {
-    $frames[] = end($frames);
-    $dauer[] = 200;
-} else {
-    die("Keine gültigen Frames erzeugt. Bitte überprüfen Sie die Logs für weitere Details."); // Diese Meldung ist jetzt sehr wichtig
-}
+// add last frame once more for double the time so that it is displayed 3x longer and thus aids dinstinguishing the end of the loop from the other frames
+$frames[] = $frames[count($frames)-1];
+$dauer[] = 200;
 
-// GIF erzeugen
-$gif = new GIFEncoder($frames, $dauer, 0, 2, 0, 0, 0, 'bin');
+// Build GIF
+$gif = new GIFEncoder($frames,$dauer,0,2,0,0,0,'bin');
+// put it out there
 echo $gif->GetAnimation();
 ?>
