554e14040411c8d83b3fe030749bbe0b711c8ac3
mammutor <mammutor@gmail.com>
2025-09-23 16:44:01 +0200

    changed weather url to new GEO1 Dach Weather URL

--- php/wetter.php
+++ php/wetter.php
@@ -1,5 +1,5 @@
 <?php
-  $url = "https://www.uni-muenster.de/Klima/";
+  $url = "https://www.uni-muenster.de/BAI/weather/";
    
   if (! $input = @file_get_contents($url))
   {
@@ -7,14 +7,29 @@
   }
   else
   { 
-    preg_match('~<table class="news_weather" summary="Aktuelles Wetter">.*Lokalzeit</th> <td>(\d\d:\d\d)</td>.*Temperatur</th> <td>(-?\d?\d\.?\d?).*Luftfeuchte</th> <td>(\d\d0?).*Beaufort</span>: (\d\d?).*Windrichtung</th> <td>aus (\w\w?\w?).*Luftdruck</th> <td>(\d\d\d\d?\.?\d?).*</table>~si', $input, $wetterdaten);
+    preg_match('~<table class="weather">.*?Messzeit.*?(\d{2}\.\d{2}\.\d{4} \d{2}:\d{2}).*?Lufttemperatur.*?(-?\d+\.?\d*).*?Relative Luftfeuchte.*?(\d+).*?Luftdruck.*?(\d+\.?\d*).*?entspricht Windstärke.*?Beaufort (\d+).*?Windrichtung.*?aus (\w+).*?</table>~si', $input, $wetterdaten);
     
-    $uhrzeit      = $wetterdaten[1];
-    $temperatur   = $wetterdaten[2];
-    $luftfeuchte  = $wetterdaten[3];
-    $windstaerke  = $wetterdaten[4];
-    $windrichtung = $wetterdaten[5];
-    $luftdruck    = $wetterdaten[6];        
+    // Prüfe ob der Regex erfolgreich war
+    if (count($wetterdaten) >= 7) {
+      // Extrahiere nur die Uhrzeit aus dem Datum
+      $datum_zeit = $wetterdaten[1];
+      preg_match('/(\d{2}:\d{2})/', $datum_zeit, $zeit_match);
+      $uhrzeit = isset($zeit_match[1]) ? $zeit_match[1] : "--:--";
+      
+      $temperatur   = $wetterdaten[2];
+      $luftfeuchte  = $wetterdaten[3];
+      $luftdruck    = $wetterdaten[4];
+      $windstaerke  = $wetterdaten[5];
+      $windrichtung = $wetterdaten[6];
+    } else {
+      // Fallback-Werte wenn Regex fehlschlägt
+      $uhrzeit      = "--:--";
+      $temperatur   = "--";
+      $luftfeuchte  = "--";
+      $windstaerke  = "--";
+      $windrichtung = "--";
+      $luftdruck    = "--";
+    }
   }
   
   echo"<strong>Aktuelles Wetter vom Dach des GEO1</strong> (Stand $uhrzeit Uhr)<br />Temperatur: <strong>$temperatur&nbsp;°C</strong> &nbsp;&bull;&nbsp; Luftfeuchte: <strong>$luftfeuchte&nbsp;%</strong> &nbsp;&bull;&nbsp; Wind: <strong>$windstaerke&nbsp;bft aus $windrichtung</strong>";
